4c03aa30d35d1b65013ef70d4b8389207e11e0bc
Michael King <sciencegrandy@gmail.com>
2025-08-28 12:06:28 +0100

    modified

--- php/connect_db.php
+++ php/connect_db.php
@@ -1,11 +1,11 @@
 <?php
-$servername = 'localhost';
-$db_user = 'admin';
-$db_password = 'great.admin';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
 $db_name = 'cell_tracker';
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$db_name;charset=utf8", $db_user, $db_password);
+  $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $user, $pass);
   // Set PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // Optional: Set default fetch mode to associative arrays
