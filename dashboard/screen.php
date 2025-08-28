6625cb95174ad1d030d8d3eaff0ba967301166ae
Michael King <sciencegrandy@gmail.com>
2025-08-29 00:03:55 +0100

    modified

--- dashboard/screen.php
+++ dashboard/screen.php
@@ -98,7 +98,9 @@
           <span class="search-icon position-absolute" style="top: 50%; right: 0; transform: translateY(-50%); padding: 0; padding-right: 12px">
             <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill=""><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
           </span>
-          <span class="loader d-none"></span>
+          <span class="loader d-none">
+            <i class="bi bi-arrow-repeat" style="animation:spin 0.8s linear infinite; display:inline-block;"></i>
+          </span>
         </div>
 
         <div class="cells-table-container w-100 p-0 m-0">
