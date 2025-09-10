ac0ab44837d871e297eb45eaff147b36fb96de10
Michael King <sciencegrandy@gmail.com>
2025-09-10 14:36:30 +0100

    modified

--- footer.php
+++ footer.php
@@ -4,10 +4,18 @@ class Footer {
     ?>
     </div> <!-- Close container-fluid -->
 
-    <!-- JQuery Local File -->
-    <script src="<?php echo BASE_URL; ?>assets/js/jquery.min.js"></script>
-    <!-- Bootstrap javascript Local File -->
-    <script src="<?php echo BASE_URL; ?>assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- JQuery CDN -->
+    <script
+      src="https://code.jquery.com/jquery-3.7.1.min.js"
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+      crossorigin="anonymous"
+    ></script>
+    <!-- Bootstrap CDN -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+      crossorigin="anonymous"
+    ></script>
     <!-- Javascript File -->
     <?php if ($isLoggedIn) {
       echo '<script src="' . BASE_URL .'script.js"></script>';
